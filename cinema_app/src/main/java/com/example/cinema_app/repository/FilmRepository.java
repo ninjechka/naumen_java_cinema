4b690b5c263cf718578858267471230e76d05d9d
ninjechka <lizko170@gmail.com>
2023-12-21 23:53:21 +0500

    work

--- cinema_app/src/main/java/com/example/cinema_app/repository/FilmRepository.java
+++ cinema_app/src/main/java/com/example/cinema_app/repository/FilmRepository.java
@@ -2,7 +2,7 @@ package com.example.cinema_app.repository;
 
 
 import com.example.cinema_app.models.Film;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +10,7 @@ import java.util.Optional;
 /**
  * Репозиторий для фильмов
  */
-public interface FilmRepository extends CrudRepository<Film, Long> {
+public interface FilmRepository extends JpaRepository<Film, Long> {
     Optional<Film> findByFilmId(Long id);
     List<Film> findAll();
     Film findByFilmName(String filmName);
--- cinema_app/src/main/java/com/example/cinema_app/repository/UserRepository.java
+++ cinema_app/src/main/java/com/example/cinema_app/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.cinema_app.repository;
 
 import com.example.cinema_app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Репозиторий для пользователей
  */
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
